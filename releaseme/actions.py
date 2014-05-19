30f59752793fb756b417a48686b3b2db8968646a
Miguel Angel Garcia <miguelangel.garcia@gmail.com>
2014-05-19 20:07:09 +0200

    Refactoring again

--- releaseme/actions.py
+++ releaseme/actions.py
@@ -4,22 +4,25 @@ from .version import Version
 
 class Actions(object):
     def increment(self, options):
-        version = None
+        version = self._get_higher_version(options)
+        version.increment()
+        self._set_version(options, version)
+        return version
 
+    def _get_higher_version(self, options):
+        version = None
         for filename in options.file:
             version = max(version,
                           self._get_version_from_file(filename))
-
-        version.increment()
-
-        for filename in options.file:
-            self._save_version_to_file(filename, version)
-
         return version
 
     def _get_version_from_file(self, filename):
             with open(filename) as fd:
                 return Version(fd.read().decode('utf-8'))
+
+    def _set_version(self, options, version):
+        for filename in options.file:
+            self._save_version_to_file(filename, version)
 
     def _save_version_to_file(self, filename, version):
         with open(filename, 'wt') as fd:
