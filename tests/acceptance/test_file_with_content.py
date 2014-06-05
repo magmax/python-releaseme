08ee83733fd0a91072d4e45739d8bf865e828c24
Miguel Angel Garcia <miguelangel.garcia@gmail.com>
2014-06-05 08:28:41 +0200

    Adding test to check files with several lines

--- tests/acceptance/test_file_with_content.py
+++ tests/acceptance/test_file_with_content.py
@@ -16,3 +16,16 @@ class FileWithContent(FileAcceptanceTest, unittest.TestCase):
         sut.expect('0.1.3', timeout=2)
         sut.wait()
         self.assertFileContent(filename, '__version__ = "0.1.3"')
+
+    def test_several_lines_file_is_maintained(self):
+        filename = self.versions_file(
+            'foo = "bar"\n__version__ = "0.1.2"\nfoo = "bar"')
+
+        sut = pexpect.spawn('python -m releaseme increment --file=%s -v'
+                            % filename)
+
+        sut.expect('0.1.3', timeout=2)
+        sut.wait()
+        self.assertFileContent(
+            filename,
+            'foo = "bar"\n__version__ = "0.1.3"\nfoo = "bar"')
